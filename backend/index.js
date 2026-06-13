3e97ae0f698dcf8499bd49763b62e7b164f214d5
Akshay Namdev <akshaynamdev963@gmail.com>
2026-06-13 23:28:01 +0530

    Day 2: Added authentication APIs with JWT

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,17 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
+const authRoutes = require("./routes/authRoutes");
 app.use(cors());
 app.use(express.json());
+const connectDB = require("./config/db");
+connectDB();
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Shree Kitchen API Running 🚀");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
